e08653607c39cc939c4066b79e891f453d4c3d50
cesar mota <cesarmota@ymail.com>
2019-12-23 01:07:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,18 @@ def monopoly_with_third_tier
   monopoly={
     :railroads=>{
       :pieces=>4,
-      :rent_in_dollars=>{:one_piece_owned=>25,:two_pieces_owned=>50,:three_pieces_owned=>100,:four_pieces_owned=>200},
+      :rent_in_dollars=>{
+        :one_piece_owned=>25,
+        :two_pieces_owned=>50,
+        :three_pieces_owned=>100,
+        :four_pieces_owned=>200},
       :names=>{
-        :reading_railroad=>{"mortgage value"=>"$100"},:pennsylvania_railroad=>{"mortgage value"=>""},:b_and_o_railroad=>{"mortgage value"=>""},:shortline_railroad=>{"mortgage value"=>""}}
+        :reading_railroad=>{"mortgage value"=>"$100"},
+        :pennsylvania_railroad=>{"mortgage value"=>"200"},
+        :b_and_o_railroad=>{"mortgage value"=>""},
+        :shortline_railroad=>{"mortgage value"=>""}
+        
+      }
   }
   }
 end
